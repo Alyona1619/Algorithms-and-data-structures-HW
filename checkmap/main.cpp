758939e3cdf3c77ac32d1cdeeb2359f95fcaf715
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-03-05 02:38:23 +0800

    Update main.cpp

--- checkmap/main.cpp
+++ checkmap/main.cpp
@@ -7,27 +7,28 @@ int main(){
 	std::map <int, int> my_map;
 
     for (int i{}; i <= 7; i++)
+    {
+	my_map.clear();
+
+	int amount = std::pow(10, i);
+
+	auto begin = std::chrono::high_resolution_clock::now();
+
+	for(int j{}; j < amount; j++)
 	{
-		my_map.clear();
-
-		int amount = std::pow(10, i);
-
-		auto begin = std::chrono::high_resolution_clock::now();
-
-		for(int j{}; j < amount; j++)
-		{
-        	my_map.insert({j, j});
+        my_map.insert({j, j});
     	}
 
-		auto finish = std::chrono::high_resolution_clock::now();
+	auto finish = std::chrono::high_resolution_clock::now();
 
 		
 
-		auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(finish - begin);
-		double b = (2 * sizeof(int) * my_map.size() + sizeof(my_map)) / 1024.0;
-		std::cout << elapsed_ms.count() << " " << (2 * sizeof(int) * my_map.size() + sizeof(my_map)) / 1024.0 << " "; //kb
+	auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(finish - begin);
+	double b = (2 * sizeof(int) * my_map.size() + sizeof(my_map)) / 1024.0;
+	std::cout << elapsed_ms.count() << " " << (2 * sizeof(int) * my_map.size() + sizeof(my_map)) / 1024.0 << " "; //kb
     }
     std::cout << std::endl;
 	return 0;
 }
+
 
