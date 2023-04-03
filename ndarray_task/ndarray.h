b37dfd176ddb3851713e2aef59498bcec8a9498e
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-04-04 00:19:40 +0800

    Update ndarray.h

--- ndarray_task/ndarray.h
+++ ndarray_task/ndarray.h
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <cassert>
+#include <numeric>
 
+// change assert to if
+// add div
+
+//add operators for each element by one number
 template <typename T> class NDArray {
 
     private:
@@ -63,41 +69,79 @@ template <typename T> class NDArray {
         };
 
         NDArray<T>& operator += (const NDArray<T>& arr) {
-            assert(shape == v.shape);
-            for (int i{}; i < len; i++) 
+            assert(shape == arr.shape);
+            for (int i{}; i < len; i++){
                 array[i] += arr.array[i];
+            }
             return *this;
-        }
+        };
 
-        NDArray<T>& operator + (const NDArray<T>& arr) {
-            return NDArray(*this) += arr;
-        }
+        NDArray<T> operator + (const NDArray<T>& arr) const{
+            return NDArray<T>(*this) += arr;
+        };
 
         NDArray<T>& operator *= (const NDArray<T>& arr) {
             assert(shape == arr.shape);
-            for (int i = 0; i < len; i++)
+            for (int i{}; i < len; i++){
                 array[i] *= arr.array[i];
+            }
             return *this;
-    }
+        };
 
         NDArray<T> operator * (const NDArray<T>& arr) const {
-            NDArray<T> res(*this);
-            return res *= arr;
-        }
+            return NDArray<T>(*this) *= arr;
+        };
 
-        NDArray<T> operator -() const {
-            NDArray<T> res(*this);
-            return res *= -1;
-        }
+        // NDArray<T> operator *= (T a) { //  на число
+        //     std::cout << a <<"00000000" << std::endl;
+        //     for (int i{}; i < len; i++){
+                
+        //         //std::cout << array[i] << "!" << std::endl;
+        //         array[i] *= a;
+                
+        //         std::cout << array[i] <<"!!!" << std::endl;
+        //     }
+        //     return *this;
+        // };
+
+        // NDArray<T> operator -() const {
+        //     return NDArray<T>(*this) *= -1;
+        // };
 
         NDArray<T>& operator -= (const NDArray<T>& arr) {
-            return (*this += (-v))
-        }
+            assert(shape == arr.shape);
+            for (int i{}; i < len; i++){
+                array[i] -= arr.array[i];
+            }
+            return *this;
+        };
 
-        NDArray<T>& operator - (const NDArray<T>& arr) {
-            NDArray<T> res(*this);
-            return res -= v;
-        }
+        NDArray<T> operator - (const NDArray<T>& arr) const{
+            return NDArray<T>(*this) -= arr;
+        };
+
+
+        NDArray<T>& operator /= (const NDArray<T>& arr) {
+            assert(shape == arr.shape);
+            for (int i{}; i < len; i++)
+                array[i] /= arr.array[i];
+            return *this;
+        };
+
+        NDArray<T> operator / (const NDArray<T>& arr) const {
+            return NDArray<T>(*this) /= arr;
+        };
+
+        NDArray<T>& operator = (const NDArray<T>& arr) {
+            array.clear();
+            len = arr.len;
+            shape = arr.shape;
+            if (arr.array.empty()) 
+                return *this;
+            for (int i{}; i < len; i++)
+                array.push_back(arr.array[i]);
+            return *this;
+        };
 
         friend std::ostream& operator << (std::ostream& out, const NDArray& arr) {
             for (auto v: arr.array){
@@ -107,4 +151,84 @@ template <typename T> class NDArray {
             return out;
         };
 
-};        
+        T& operator() (int i) {
+            assert((i < len));
+            return array[i];
+        };
+
+
+        T& operator() (int i, int j) {
+            assert(i < shape[0]);
+            assert(j < shape[1]);
+            return array[i * shape[1] + j];
+        };
+
+        NDArray<T>& transpose(){
+            NDArray<T> res({shape[1], shape[0]});
+            for (int i{}; i < shape[0]; i++) {
+                for (int j{}; j < shape[1]; j++) {
+                    res(j, i) = (*this)(i, j);
+                }
+            }
+            *this = res;
+            return *this;
+        };
+
+        NDArray<T> matrix_multi(const NDArray<T> arr){
+            assert(shape[1] == arr.shape[0]);
+            assert(shape[0] == arr.shape[1]);
+
+            NDArray<T> res({shape[0], arr.shape[1]});
+            NDArray<T> clon;
+            clon = arr;
+            clon.transpose();
+
+            for (int k{}; k < shape[0]; k++) {
+                for (int i{}; i < clon.shape[0]; i++) {
+                    T n = 0;
+                    for (int j{}; j < shape[1]; j++) {
+                        n += (*this)(k, j) * clon(i, j);
+                    }
+                    res(k, i) = n;
+                }
+            }
+            return res;
+        };
+
+        T min(){
+            if (array.empty()){
+                return 0;
+            }
+            else{
+                T min = array[0];
+                for (auto i : array)
+                    if (i < min)
+                        min = i;
+                return min;
+            }
+        };
+
+        T max() {
+            if (array.empty()){
+                return 0;
+            }
+            else{
+                T max = array[0];
+                for (auto i : array)
+                    if (i > max)
+                        max = i;
+                return max;
+            }
+        };
+
+        T mean(){
+            if (array.empty()){
+                return 0;
+            }
+            else{
+                T mean = std::accumulate(array.begin(), array.end(), 0.0) / array.size();
+                return mean;
+            }
+        };
+
+};              
